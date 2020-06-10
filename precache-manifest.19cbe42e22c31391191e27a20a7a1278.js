70a861652953f67cf2843a3685d70284b18200e3
soundharya04 <souns14@gmail.com>
2020-06-10 18:28:45 +0530

    Updates

---
+++ precache-manifest.19cbe42e22c31391191e27a20a7a1278.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18c9dc348ec60b6a6af7bd8eaa454fa",
+    "url": "/portfoliov2/index.html"
+  },
+  {
+    "revision": "67919148d47dedc80d5d",
+    "url": "/portfoliov2/static/css/main.b111c081.chunk.css"
+  },
+  {
+    "revision": "f36c77a34fd5d8ed6508",
+    "url": "/portfoliov2/static/js/2.5d351f0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfoliov2/static/js/2.5d351f0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67919148d47dedc80d5d",
+    "url": "/portfoliov2/static/js/main.f03965f3.chunk.js"
+  },
+  {
+    "revision": "7f37d128ceefade70397",
+    "url": "/portfoliov2/static/js/runtime-main.7d5e15c2.js"
+  },
+  {
+    "revision": "52bec8f6a5fb989a25429cd17b52c036",
+    "url": "/portfoliov2/static/media/about-us.52bec8f6.png"
+  },
+  {
+    "revision": "183584bb46a7ac5095433be7f5092f37",
+    "url": "/portfoliov2/static/media/designer.183584bb.jpg"
+  }
+]);
